7eabf1b46e28c5878774aa4f76641fdb8dec92bf
Aleksej Voroncov <a.voroncovs@bill.me>
2019-12-12 18:49:59 +0200

    cors config for 'development' profile

--- src/main/java/com/example/mnclone/config/SecurityConfiguration.java
+++ src/main/java/com/example/mnclone/config/SecurityConfiguration.java
@@ -3,12 +3,15 @@ package com.example.mnclone.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -21,14 +24,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
-                .cors().disable()
+                .cors().and()
                 .formLogin().successHandler((request, response, authentication) -> response.setStatus(200))
                 .and()
                 .csrf().disable();
     }
 
+    @Bean
+    @Profile("development")
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowCredentials(true);
+            }
+        };
+    }
+
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(swCloneAuthenticationProvider);
     }
 
