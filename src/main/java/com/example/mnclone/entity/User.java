d88f6bf91f4038d6144b2932204ef093499e8f07
Aleksej <avoroncovs@gmail.com>
2019-12-14 15:46:22 +0200

    mailcatcher port mapping

--- src/main/java/com/example/mnclone/entity/User.java
+++ src/main/java/com/example/mnclone/entity/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import java.math.BigDecimal;
 
 @Data
@@ -26,6 +27,7 @@ public class User {
     @Column(nullable = false)
     private String lastName;
     @Column(nullable = false)
+    @Min(0)
     private BigDecimal balance;
     @Column(nullable = false)
     private Boolean registered;
